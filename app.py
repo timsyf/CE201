f70a8671f0c408f740db18df827fb6ea8aace68b
Timothy Soon <timothysoonyufeng@gmail.com>
2024-02-04 04:02:38 +0800

    Adjust database, create Create/Read/Delete for User, more styling with tailwind

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# SELECT ALL USER
+@app.route('/user', methods=['GET'])
+def user():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    cursor.execute('SELECT * FROM User')
+    users = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('User/index.html', users=users)
+
+# INSERT USER
+@app.route('/user/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute('INSERT INTO User (name) VALUES (?)', (name,))
+
+        conn.commit()
+        conn.close()
+
+    return redirect(url_for('user'))
+
+# DELETE USER
+@app.route('/delete/<int:staff_id>', methods=['POST'])
+def delete(staff_id):
+    if request.method == 'POST':
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute('DELETE FROM User WHERE id = ?', (staff_id,))
+
+        conn.commit()
+        conn.close()
+
+    return redirect(url_for('user'))
--- create_table.py
+++ create_table.py
@@ -3,7 +3,12 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
-conn.execute('CREATE TABLE staff (name TEXT)')
+conn.execute('''
+    CREATE TABLE User (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+''')
 print("Created table successfully!")
 
 conn.close()
